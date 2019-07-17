13ba033775f9c15fd04b1ac0b19f7d34bf93bd5c
Giovani de Oliveira <giovanioliveira@outlook.com.br>
2019-07-16 22:29:15 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Klaffer
+
+A Google Chrome extension for manage tabs.
+
+![Menu](http://i.imgur.com/8X4jOgk.png)
+
+## Options
+
+1. Close current tab
+2. Close all tabs
+3. Close tabs to the left of current tab
+4. Close tabs to the right of current tab
+5. Close other tabs except current tab
+6. Close tabs from current domain 
+7. Close tabs from other domain
+8. Close tabs with sound
+9. Mute tabs with sound
+10. Unmute tabs with sound
+11. Merge windows
+
+The options is available on right click
+
+## Run
+
+```
+# Clone the repository
+git clone https://github.com/xxgicoxx/klaffer.git
+
+# Open Google Chrome extensions
+At the top right, turn on Developer mode
+
+# Load
+Click Load unpacked
+
+# Select
+Find and select the Klaffer folder
+```
+
+## Generate version
+
+```
+# Find the Klaffer folder
+Zip the folder. Then, Publish it in the Chrome Web Store.
+```
---
+++ js/background.js
@@ -0,0 +1,169 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        title: "Klaffer",
+        id: "parent",
+        contexts:["all"]
+    });
+
+    chrome.contextMenus.create({
+        title: "Close current tab",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({active: true}, function (tabs) {
+                chrome.tabs.remove(tabs[0].id);
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close all tabs",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({}, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    chrome.tabs.remove(tabs[i].id);
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close tabs to the left of current tab",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({}, function (tabs) {
+                let i = 0;
+                while(!tabs[i].active){
+                    chrome.tabs.remove(tabs[i].id);
+                    i++;
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close tabs to the right of current tab",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({}, function (tabs) {
+                let close = false;
+                for (var i = 0; i < tabs.length; i++) {
+                    if(tabs[i].active){
+                        close = true;
+                    } else if (close) {
+                        chrome.tabs.remove(tabs[i].id);
+                    }
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close other tabs except current tab",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({active: false}, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    chrome.tabs.remove(tabs[i].id);
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close tabs from current domain",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({active: true}, function (tab) {
+                let url = new URL(tab[0].url);
+                let domain = url.hostname
+                chrome.tabs.query({}, function (tabs) {
+                    for (var i = 0; i < tabs.length; i++) {
+                        if(tabs[i].url.includes(domain)){
+                            chrome.tabs.remove(tabs[i].id);
+                        }
+                    }
+                });
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close tabs from other domain",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({active: true}, function (tab) {
+                let url = new URL(tab[0].url);
+                let domain = url.hostname
+                chrome.tabs.query({}, function (tabs) {
+                    for (var i = 0; i < tabs.length; i++) {
+                        if(!tabs[i].url.includes(domain)){
+                            chrome.tabs.remove(tabs[i].id);
+                        }
+                    }
+                });
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Close tabs with sound",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({audible: true}, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    chrome.tabs.remove(tabs[i].id);
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Mute tabs with sound",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({audible: true}, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    chrome.tabs.update(tabs[i].id, {"muted": true});
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Unmute tabs with sound",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({muted: true}, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    chrome.tabs.update(tabs[i].id, {"muted": false});
+                }
+            });
+        }
+    });
+
+    chrome.contextMenus.create({
+        title: "Merge windows",
+        parentId: "parent",
+        contexts:["all"],
+        onclick: function(info){
+            chrome.tabs.query({active: true, highlighted: true}, function (tab) {
+                chrome.tabs.query({}, function (tabs) {
+                    for (var i = 0; i < tabs.length; i++) {
+                        chrome.tabs.move(tabs[i].id, {windowId: tab[0].windowId, index: -1}, function (){});
+                    }
+                });
+            });
+        }
+    });
+});
